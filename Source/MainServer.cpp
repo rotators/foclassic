92e582fbe67c2c4677702dd0920f7e72af3424b2
Wipe <wipe.git@gmail.com>
2018-10-27 19:28:49 +0200

    v1

--- Source/MainServer.cpp
+++ Source/MainServer.cpp
@@ -177,7 +177,7 @@ int main( int argc, char** argv )
 
     WriteLog( "FOClassic server, version %u.\n", FOCLASSIC_VERSION );
 
-    # if 1
+    # if 0
     WriteLog( "STATIC_ASSERT\n" );
     STATIC_ASSERT_PRINTF( WriteLog, char );
     STATIC_ASSERT_PRINTF( WriteLog, short );
