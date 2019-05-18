8df305ac09618dd5958a931098c4c06479f1767a
Wipe <wipe.git@gmail.com>
2019-05-18 10:34:42 +0200

    appveyor

--- Source/Extension.Helper.h
+++ Source/Extension.Helper.h
@@ -2,26 +2,37 @@
 #define __EXTENSION_HELPER__
 
 //
-// This file should be included after regular headers
+// This file should be included as last (after other engine headers)
 //
 
 #undef EXPORT
 #define EXPORT
 
+// CritterMutual = Critter, if Critter.h was included
 #if defined (FOCLASSIC_SERVER) && defined (__CRITTER__)
 typedef Critter     CritterMutual;
 #endif
 
+// CritterMutual = Critter, if CritterCl.h was included
 #if !defined (FOCLASSIC_SERVER) && defined (__CRITTER_CL__)
 typedef CritterCl   CritterMutual;
 #endif
 
+// ItemCl = Item, if ItemCl.h was included
 #if !defined (FOCLASSIC_SERVER) && defined (__ITEM_CL__)
 typedef Item        ItemCl;
 #endif
 
+// NpcPlane = AIDataPlane, if AI.h was included
 #if defined (FOCLASSIC_SERVER) && defined (__AI__)
 typedef AIDataPlane NpcPlane;
 #endif
+
+// some functions in FOClassic.h has been copied from "Shared" library, available for all targets...
+#include "Core.h"
+
+// ...however they use different names
+
+#define GetDistantion(x1,y1,x2,y2) DistGame(x1,y1,x2,y2)
 
 #endif // __EXTENSION_HELPER__ //
